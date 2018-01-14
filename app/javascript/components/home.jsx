978ed7a51642707f2d00f756f676650ec1678ef7
Tri Vuong <trivektor@gmail.com>
2018-01-14 02:48:57 -0800

    setup Root component

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div>Home Page</div>
     );
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import Home from 'components/home';
+import Root from 'components/root';
 import WebpackerReact from 'webpacker-react';
 
-WebpackerReact.setup({ Home });
+WebpackerReact.setup({ Root });
